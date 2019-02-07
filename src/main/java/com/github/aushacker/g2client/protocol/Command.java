a69881cdef63faab22225ec4d28e3de7ff183a21
Stephen Davies <sjdavies2@icloud.com>
2019-02-07 12:59:23 +1100

    Cleanup project copyright text.

--- src/main/java/com/github/aushacker/g2client/protocol/Command.java
+++ src/main/java/com/github/aushacker/g2client/protocol/Command.java
@@ -1,22 +1,21 @@
 /*
  * Copyright 2018 Stephen Davies
  *
- * This file is part of Eagle2nc.
+ * This file is part of g2client.
  *
- * Eagle2nc is free software: you can redistribute it and/or modify
+ * g2client is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Eagle2nc is distributed in the hope that it will be useful,
+ * g2client is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Eagle2nc. If not, see <https://www.gnu.org/licenses/>.
+ * along with g2client. If not, see <https://www.gnu.org/licenses/>.
  */
-
 package com.github.aushacker.g2client.protocol;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -36,6 +35,11 @@ public abstract class Command implements Comparable<Command> {
 	}
 
 	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
 		return id;
 	}
 }
